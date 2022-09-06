96dfcc912ad0ecd89bf1ccbcaba04c293f64fe85
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-09-06 14:03:27 +0530

    added a meetup detail page and component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="asdasdads"
+      address="asdasd"
+      description="asdasd"
+    />
+  );
+};
+
+export default MeetupDetails;
