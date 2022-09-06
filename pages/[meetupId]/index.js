dbcb73c5b6004053d4042f41b6cd5f119d09ea0f
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-09-06 15:08:27 +0530

    added getStaticPaths & Working With Fallback Pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,43 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = (context) => {
+  // featch the data from single meetup
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "asdasdads",
+        address: "asdasd",
+        description: "asdasd",
+      },
+    },
+    revalidate: 1,
+  };
+};
+
 export default MeetupDetails;
